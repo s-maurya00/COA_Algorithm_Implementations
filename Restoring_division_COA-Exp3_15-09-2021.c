30b4fc9d7af1f1bd6abf93b1da82a5d424eb0f3b
Sushil <mauryasushil045@gmail.com>
2021-09-17 19:25:21 +0530

    added restoring division blank code template

---
+++ Restoring_division_COA-Exp3_15-09-2021.c
@@ -0,0 +1,16 @@
+// Date : 17-09-2021   Status: Incomplete
+
+//Program a code for restoring division
+
+void DeciToBin();
+void BinToDeci();
+void ShiftLeft();
+void AddBin();
+
+int Accumulator[50], M_bin[50], M_TwosComp[50], Q_bin[50], Q_TwosComp[50];
+int count = 0, Q_minus1 = 0;
+
+void main()
+{
+    
+}
